3548ac8bb1bf4031747759be67867317de941a3e
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-20 05:04:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "29245477"))
+API_HASH = environ.get("API_HASH", "0abc83883262245c90ca337b7a0375c4")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002456565415"))
+ADMINS = int(environ.get("ADMINS", "7654385403"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+DB_URI = environ.get("DB_URI", "mongodb+srv://Kafka:Au3OoWzCDYJKeuHU@cluster0.lz2m8iy.mongodb.net/") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = environ.get("DB_NAME", "cluster0")
 
 # If this is True Then Bot Accept New Join Request 
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
