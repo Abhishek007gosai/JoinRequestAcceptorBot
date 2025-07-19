0f3e8192e29d1ad82dc77b73a40b543ffc21ad1a
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-20 05:19:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29245477"))
 API_HASH = environ.get("API_HASH", "0abc83883262245c90ca337b7a0375c4")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7404600334:AAEn3yC-YFYtdJ1IMdLa0UG6dv3y0w1Xzvs")
 
 # Make Bot Admin In Log Channel With Full Rights
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002456565415"))
